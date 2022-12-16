0f6e7e44df6a74138bd21ff0434fc119923fdea4
Sergey <aliger99@rambler.ru>
2022-12-16 08:18:28 +0700

    SEM_02_HOME_7

--- src/main/java/org/example/model/Main_home_03.java
+++ src/main/java/org/example/model/Main_home_03.java
@@ -1,24 +1,26 @@
 package org.example.model;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main_home_03 {
 
     public static void main(String[] args) {
         //Дана последовательность N целых чисел. Найти сумму простых чисел.
-        int a, b, sum=0, n;
+        int a, sum=0, n;
+        System.out.println("Введите количество чисел в последовательности: ");
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
-        a = scanner.nextInt();
+        System.out.println("Введите последовательность: ");
         for (int i = 0; i < n; i++) {
-            //b = scanner.nextInt();
-            if ((a > 0) && (a % 2 == 0)) {
+            a = scanner.nextInt();
+            BigInteger bigInteger = BigInteger.valueOf(a);
+            boolean probablePrime = bigInteger.isProbablePrime((int) Math.log(a));
+            if (probablePrime) {
                 sum+=a;
             }
-            //a=b;
-
         }
-        System.out.println("sum= " +sum);
+        System.out.println("Сумма простых чисел = " +sum);
         scanner.close();
     }
 
